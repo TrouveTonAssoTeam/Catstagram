87947f7b8d5bd37f99a6a1852b33fd7044d29c7e
ShonaNortes <shonanortes.unterwald@gmail.com>
2023-11-27 15:48:50 +0100

    Add table item, model, controller, view and config routes

---
+++ app/controllers/item_controller.rb
@@ -0,0 +1,17 @@
+class ItemController < ApplicationController
+
+    def index
+        @items = Item.all
+    end
+
+    def show
+
+    end
+
+    private
+
+      def to_param
+
+      end
+
+end
