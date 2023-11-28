ea2914b1ac3232a67474f982987e024823890f0b
LoulergueC <clement.loulergue@live.fr>
2023-11-28 11:04:12 +0100

    Routes and item index change to match the table db and to be able to display correctly the seed with no error

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root 'item#index'
-  get 'show' => 'item#show'
+  resources :item, only: [:index, :show]
 
   scope '/payment' do
     post 'create', to: 'payment#create', as: 'checkout_create'
