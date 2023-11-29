5f9883b137d5d0cfd3d0bfdbf935e5fb7abe8b76
GuillaumeYves <guillaume-las@hotmail.fr>
2023-11-29 19:58:42 +0100

    Active storage only

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,29 @@
+class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item, notice: 'Photo ajoutée!.'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :tag, :image)
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
     has_many :orders
     has_many :carts
+    has_one_attached :image
 end
