352fbf87cac418b364dd0b65570a63109d8622b5
LoulergueC <clement.loulergue@live.fr>
2023-11-29 16:41:47 +0100

    After order create callback send email

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ApplicationRecord
+    after_create :send_confirmation_email
+
     belongs_to :user 
     has_many :items
+
+    def send_confirmation_email
+        OrderMailer.confirm_order(current_user, self).deliver_now
+        OrderMailer.new_order_admin(self).deliver_now
+    end
 end
