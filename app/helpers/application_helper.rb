1733c11b0d0022773889a427f37fceda7101916a
LoulergueC <clement.loulergue@live.fr>
2023-11-30 16:56:44 +0100

    fix static pages controller error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,4 @@ module ApplicationHelper
     def is_active?(link_path)
         current_page?(link_path) ? "active" : ""
     end
-
-    def create_cart_and_assign_to_current_user
-        if current_user != nil && current_user.cart == nil
-            cart = Cart.create(user: current_user)
-        end
-    end
 end
